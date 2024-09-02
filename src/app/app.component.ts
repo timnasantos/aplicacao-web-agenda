5014d86fe336d0a5f3414139b955c652c286b07e
timnasantos <timnasantosdev@gmail.com>
2024-09-02 19:51:11 -0300

    Remover acentos na busca de contatos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,18 +36,22 @@ export class AppComponent {
 
   filtroPorTexto: string =''
 
+  private removerAcentos(texto: string): string {
+    return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, ''); 
+  }
+
   filtrarContatosPorTexto (): Contato[] {
     if (!this.filtroPorTexto) {
       return this.contatos
     }
     return this.contatos.filter(contato => {
-      return contato.nome.toLowerCase().includes(this.filtroPorTexto.toLowerCase())
+      return this.removerAcentos(contato.nome).toLowerCase().includes(this.removerAcentos(this.filtroPorTexto).toLowerCase())
     })
   }
 
   filtrarContatosPorLetraInicial(letra:string) : Contato[] {
     return this.filtrarContatosPorTexto().filter( contato => {
-      return contato.nome.toLowerCase().startsWith(letra)
+      return this.removerAcentos(contato.nome).toLowerCase().startsWith(this.removerAcentos(letra).toLowerCase());
     })
   }
 }
